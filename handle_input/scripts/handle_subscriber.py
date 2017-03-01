b33bdc1d6e3312befdfcc2651f5f317422613509
amccormick <joshsayavong@hotmail.com>
2017-03-01 01:05:39 -0500

    created ros package for handle_input

---
+++ handle_input/scripts/handle_subscriber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from handle_input.msg import StressState
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + ": I heard  %s", data.data)
+
+def listener():
+    rospy.init_node('handle_subscriber', anonymous=True)
+    rospy.Subscriber('handle', StressState, callback)
+    rospy.loginfo("Listener is now on.")
+
+    rospy.spin()
+
+if __name__ == "__main__":
+    listener()
