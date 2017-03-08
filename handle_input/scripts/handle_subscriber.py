72364ed6159bd08b82b0f6796f0e9991a0390a50
joshuasayavong <joshsayavong@hotmail.com>
2017-03-08 14:00:00 -0500

    Fixed compilation errors

--- handle_input/scripts/handle_subscriber.py
+++ handle_input/scripts/handle_subscriber.py
@@ -4,7 +4,7 @@ import rospy
 from handle_input.msg import StressState
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "Sensor %s :{ x:  %s, y: %s, g: %s }", str(data.id), str(data.x), str(data.y), str(data.g))
+    rospy.loginfo(rospy.get_caller_id() + "Sensor %s :{ x:  %s, y: %s, g: %s }", str(data.id), str(data.x), str(data.y), str(data.grip))
 
 def listener():
     rospy.init_node('handle_subscriber', anonymous=True)
